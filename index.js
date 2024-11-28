cfde9cbdd1194071500357992a995098aeaa3fa9
irarubrian <brian.o.iraru@gmail.com>
2024-11-28 12:53:11 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 =31
+const num2 =2
+const multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+//Random 
+const random = Math.floor(Math.random() * 100) + 0
+
+//Equal to 4 (mod)
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+console.log(mod)
+
+//max
+const max = Math.max(1,2,5,6,20)
